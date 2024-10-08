fb47c852df835ef621234611137000f979eb5862
Will Olson <will.olson.b@gmail.com>
2024-10-08 14:57:23 -0500

    Rough draft, first try

---
+++ src/00-objectball.js
@@ -0,0 +1,17 @@
+function gameObject() {
+   
+const "home" = 
+   "teamName" = {''}
+   "colors" = {''}
+   const "players" =
+    "number"
+    "shoe"
+    "points"
+    "rebounds"
+    "assists"
+    "steals"
+    "blocks"
+    "slamDunks"
+   const "away" =
+
+}
