581117d4ffe6d8753fc1ac6afe8ceaa5ae8d7778
Will Olson <will.olson.b@gmail.com>
2024-10-08 16:24:36 -0500

    Updated gameObject, pending functions.

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,140 +1,146 @@
-console.log(gameObject());
-/*
-{
-home: { 
-    teamName:"",
-    colors:[...],
-    players:{
-        "Alan Anderson"
-        Reggie Evans"
-        "Brook Lopez" 
-        "Mason Plumlee"
-        "Jason Terry"
-}
-    Numbers: {
-        "0"
-        "30"
-        "11"
-        "1"
-        "31"
-}
-    Shoes: {
-        "16"
-        "14"
-        "17"
-        "19"
-        "15"
-}
-    Points{
-        "22"
-        "12"
-        "17"
-        "26"
-        "19"
-}
-    Rebounds{
-        "12"
-        "12"
-        "19"
-        "12"
-        "2"
-}
-    Assists{
-        "12"
-        "12"
-        "10"
-        "6"
-        "2"
-}
-    Steal{
-        "3"
-        "12"
-        "3"
-        "3"
-        "3"
-        "4"
-}
-        Blocks{
-        "1"
-        "12"
-        "1"
-        "8"
-        "11"
-}
-        Slam Dunks{
-        "1"
-        "7"
-        "15"
-        "5"
-        "1"
-}
-}
+let gameObject = {
+    home: {
+      teamName: "Brooklyn Nets",
+      colors: ["black", "white"],
+      players: {
+        "Alan Anderson": {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 1,
+        },
+        "Reggie Evans": {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slamDunks: 7,
+        },
+        "Brook Lopez": {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slamDunks: 15,
+        },
+        "Mason Plumlee": {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slamDunks: 5,
+        },
+        "Jason Terry": {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slamDunks: 1,
+        },
+      },
+    },
+    away: {
+        teamName: "Charlotte Hornets",
+        colors: ["Turquoise", "Purple"],
+        players: {
+          "Jeff Adrien": {
+            number: 4,
+            shoe: 18,
+            points: 10,
+            rebounds: 1,
+            assists: 1,
+            steals: 2,
+            blocks: 7,
+            slamDunks: 2,
+          },
+          "Bismak Biyombo": {
+            number: 0,
+            shoe: 16,
+            points: 12,
+            rebounds: 4,
+            assists: 7,
+            steals: 7,
+            blocks: 15,
+            slamDunks: 10,
+          },
+          "DeSagna Diop": {
+            number: 2,
+            shoe: 21,
+            points: 24,
+            rebounds: 12,
+            assists: 12,
+            steals: 4,
+            blocks: 5,
+            slamDunks: 5,
+          },
+          "Ben Gordon": {
+            number: 8,
+            shoe: 15,
+            points: 33,
+            rebounds: 3,
+            assists: 2,
+            steals: 1,
+            blocks: 1,
+            slamDunks: 0,
+          },
+          "Brendan Haywood": {
+            number: 33,
+            shoe: 15,
+            points: 6,
+            rebounds: 12,
+            assists: 12,
+            steals: 22,
+            blocks: 5,
+            slamDunks: 12,
+          },
+        },
+      },
+    };
 
-   const "away" {
-   teamName:"",
-    colors:[...],
-    players:{
-        "Jeff Adrien"
-        "Bismak Biyombo"
-        "DeSagna Diop" 
-        "Ben Gordon"
-        "Brendan Haywood"
+// home function
+
+function homeTeamName() {
+  let object = gameObject();
+  return object["home"]["teamName"];
 }
-    Numbers: {
-        "4"
-        "0"
-        "2"
-        "8"
-        "33"
+function colors() {
+  let object = gameObject();
+  return object["home"]["colors"];
 }
-    Shoes: {
-        "18"
-        "16"
-        "14"
-        "15"
-        "15"
-}
-    Points{
-        "10"
-        "12"
-        "24"
-        "33"
-        "6"
-}
-    Rebounds{
-        "1"
-        "4"
-        "12"
-        "3"
-        "12"
-}
-    Assists{
-        "1"
-        "7"
-        "12"
-        "2"
-        "12"
-}
-    Steal{
-        "2"
-        "7"
-        "4"
-        "1"
-        "22"
-}
-        Blocks{
-        "7"
-        "15"
-        "5"
-        "1"
-        "5"
-}
-        Slam Dunks{
-        "2"
-        "10"
-        "5"
-        "0"
-        "12"
-        }
+function players() {
+  let object = gameObject();
+  return object["home"]["players"];
     }
+
+// away function
+
+function awayTeamName() {
+  let object = gameObject();
+  return object["away"]["teamName"];
+
+function colors() {
+  let object = gameObject();
+  return object["away"]["colors"];
 }
+function players() {
+  let object = gameObject();
+  return object["away"]["players"];
+
+  function numPointsScored (players) => ["Points"]
+
