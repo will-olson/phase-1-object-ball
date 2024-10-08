3383befe1d23c239c88910d23057e2413ca86c66
Karina Mogha <moghakarina@gmail.com>
2024-10-08 16:32:42 -0400

    home run tables

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,17 +1,140 @@
-function gameObject() {
-   
-const "home" = 
-   "teamName" = {''}
-   "colors" = {''}
-   const "players" =
-    "number"
-    "shoe"
-    "points"
-    "rebounds"
-    "assists"
-    "steals"
-    "blocks"
-    "slamDunks"
-   const "away" =
+console.log(gameObject());
+/*
+{
+home: { 
+    teamName:"",
+    colors:[...],
+    players:{
+        "Alan Anderson"
+        Reggie Evans"
+        "Brook Lopez" 
+        "Mason Plumlee"
+        "Jason Terry"
+}
+    Numbers: {
+        "0"
+        "30"
+        "11"
+        "1"
+        "31"
+}
+    Shoes: {
+        "16"
+        "14"
+        "17"
+        "19"
+        "15"
+}
+    Points{
+        "22"
+        "12"
+        "17"
+        "26"
+        "19"
+}
+    Rebounds{
+        "12"
+        "12"
+        "19"
+        "12"
+        "2"
+}
+    Assists{
+        "12"
+        "12"
+        "10"
+        "6"
+        "2"
+}
+    Steal{
+        "3"
+        "12"
+        "3"
+        "3"
+        "3"
+        "4"
+}
+        Blocks{
+        "1"
+        "12"
+        "1"
+        "8"
+        "11"
+}
+        Slam Dunks{
+        "1"
+        "7"
+        "15"
+        "5"
+        "1"
+}
+}
 
+   const "away" {
+   teamName:"",
+    colors:[...],
+    players:{
+        "Jeff Adrien"
+        "Bismak Biyombo"
+        "DeSagna Diop" 
+        "Ben Gordon"
+        "Brendan Haywood"
+}
+    Numbers: {
+        "4"
+        "0"
+        "2"
+        "8"
+        "33"
+}
+    Shoes: {
+        "18"
+        "16"
+        "14"
+        "15"
+        "15"
+}
+    Points{
+        "10"
+        "12"
+        "24"
+        "33"
+        "6"
+}
+    Rebounds{
+        "1"
+        "4"
+        "12"
+        "3"
+        "12"
+}
+    Assists{
+        "1"
+        "7"
+        "12"
+        "2"
+        "12"
+}
+    Steal{
+        "2"
+        "7"
+        "4"
+        "1"
+        "22"
+}
+        Blocks{
+        "7"
+        "15"
+        "5"
+        "1"
+        "5"
+}
+        Slam Dunks{
+        "2"
+        "10"
+        "5"
+        "0"
+        "12"
+        }
+    }
 }
